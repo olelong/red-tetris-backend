8badb794b82795a5498fffeecc27d8b347242975
olelong <olelong@student.42.fr>
2025-01-16 23:39:58 +0100

    Remove all

--- src/gateway/game.module.js
+++
@@ -1,6 +0,0 @@
-import { GameService } from './game.service';
-import { GameGateway } from './game.gateway';
-import { Module } from '@nestjs/common';
-
-@Module({ providers: [GameGateway, GameService] })
-export class GameModule {}
